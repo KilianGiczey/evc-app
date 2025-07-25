bedcbe94a8a404f9b5e0a7463aa9298fc43c7313
Kilian Giczey <Kilian.Giczey@Gmail.com>
2025-07-25 14:38:16 +0200

    Initial V5

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,10 @@
-module.exports = {
-  root: true,
-  extends: [
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
-  ],
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  rules: {
-    // Add custom rules here if needed
-  },
-  settings: {
-    react: {
-      version: 'detect'
+import next from 'eslint-config-next';
+
+export default [
+  ...next(),
+  {
+    rules: {
+      // Add custom rules here if needed
     }
   }
-}; 
+]; 
